e4aff7cec79b49b95adfb67776a94e9ad4ae1ab9
Ken Wagner <ken_xwagner@hotmail.com>
2018-06-11 21:26:07 -0700

    Added
    explanation and implementation of delegates

---
+++ cSharp/delegates.cs
@@ -0,0 +1,7 @@
+// a delegate is a reference type variable that holds the reference to a method. The reference can be changed at runtime.
+using System;
+
+delegate int NumberChanger(int n);
+namespace DelegateApp1 {
+
+}
