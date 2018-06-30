37a94c7541b9a93eda40f37dabd7573b56476707
Ken Wagner <ken_xwagner@hotmail.com>
2018-06-29 22:16:06 -0700

    Playtime

---
+++ Java/Practice/ControlFlow.java
@@ -0,0 +1,13 @@
+package com.built2gsd.controlflow;
+
+public class MyClas {
+	public void methodCalling(){
+		
+		add(15, 7);
+	}
+	
+		public int add (int a, int b){
+			int answer = a + b;
+			return answer;
+		}
+}
