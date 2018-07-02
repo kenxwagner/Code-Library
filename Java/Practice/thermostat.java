df73482a175e0c429e1d76ac90f32dfc6f5d6ff9
Ken Wagner <ken_xwagner@hotmail.com>
2018-07-01 19:26:47 -0700

    thermostat

---
+++ Java/Practice/thermostat.java
@@ -0,0 +1,20 @@
+package com.built2gsd.controlflow;
+
+public class MyClass {
+	
+	public void thermostat(){
+		
+		boolean isHeaterOn = false;
+		
+		int temp = 72;
+		
+		if (temp < 60) {
+			isHeaterOn = true;			
+		}else if(temp > 80 {
+			isHeaterOn = false;
+			)
+		else{
+			isHeaterOn = false;
+		}
+	}
+}
