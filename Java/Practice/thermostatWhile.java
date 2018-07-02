c81977a55eb05a758c2893b2b27b81a7ae8a3376
Ken Wagner <ken_xwagner@hotmail.com>
2018-07-01 19:26:36 -0700

    thermostatWhile

---
+++ Java/Practice/thermostatWhile.java
@@ -0,0 +1,13 @@
+package com.built2gsd.controlflow;
+
+public class MyClass {
+	
+	public void loopingThermostat(){
+		
+		int temperature = 100;
+		
+		while(temperature < 100){
+			temperature++;
+		}
+	}
+}
