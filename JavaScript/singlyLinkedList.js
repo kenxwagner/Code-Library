71bb60d447f11aa04e4e1e26ced005e512e2ae6b
Ken Wagner <ken_xwagner@hotmail.com>
2018-08-03 20:59:37 -0700

    more code

--- JavaScript/singlyLinkedList.js
+++ JavaScript/singlyLinkedList.js
@@ -2,3 +2,9 @@ function LinkedList(value, next) {
     this._value = value;
     this._next = next;
 }
+LinkedList.prototype.value = function() {
+	if (arguments.length == 1)
+		this._value = arguments[0];
+	else
+		return this._value;
+}
