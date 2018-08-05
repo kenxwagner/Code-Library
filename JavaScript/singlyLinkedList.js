5ed8727a6e6543179128556df99453b25ea1ecd1
Ken Wagner <ken_xwagner@hotmail.com>
2018-08-04 21:17:34 -0700

    updates

--- JavaScript/singlyLinkedList.js
+++ JavaScript/singlyLinkedList.js
@@ -8,3 +8,16 @@ LinkedList.prototype.value = function() {
 	else
 		return this._value;
 }
+LinkedList.prototype.next = function() {
+	if (arguments.length ==1)
+		this._next = arguments[0];
+	else
+		return this._next;
+}
+
+//creating linked list
+function createLinkedListFromArray(ary) {
+	var head = new LinkedList (ary[0], null);
+	var prev = head;
+	for (var i = 1; i < ary.length;)
+}
